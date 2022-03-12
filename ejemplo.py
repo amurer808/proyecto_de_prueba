67653a9937035985973fc2ecbefa2ed672e1d8ed
amurer808 <macarena_ru_re@hotmail.com>
2022-03-12 14:36:20 +0100

    listo para hacer push

--- ejemplo.py
+++ ejemplo.py
@@ -3,10 +3,21 @@
 if __name__ == "__main__":
     print("Con git clone se trae el proyecto desde github")
     print("Ejemplo: git clone https://github.com/amurer808/proyecto_de_prueba.git")
+    print()
 
     print("Con git status se muestra el estado de los archivos: los que estan guardados (en verde) y los que no (en rojo) (los tracked y los untracked)")
     print("Ejemplo: git status")
+    print()
 
     print("Con git add se revisan los archivos para guardarlos")
     print("Ejemplo: git add ejemplo.py")
     print("git add * : guarda todos los archivos modificados")
+    print()
+
+    print("Para registrar los cambios y hacer un estado utilizo git commit")
+    print('Ejemplo: git commit -m "Mensaje"')
+    print()
+
+    print("Para subir los cambios a github se usa el git push")
+    print("Ejemplo: git push")
+    print()
